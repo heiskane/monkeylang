a7e38b8b8c845575a37c52e4bf3a6b2be0e954ca
Niko Heiskanen <henipe@gmail.com>
2025-10-24 16:31:28 +0300

    (wip): 2025-10-24T16:31

--- lib/lexer.ex
+++ lib/lexer.ex
@@ -1,5 +1,19 @@
 defmodule Monkeylang.Lexer do
-  def hello() do
-    :potato
+  defstruct [:input, position: 0, readPosition: 0, ch: 0]
+
+  def new(input) do
+    %__MODULE__{input: input}
+  end
+
+  def parse(input) do end
+
+  def read_char(%__MODULE__{} = lexer) do
+    dbg(lexer)
+    {
+      lexer
+      |> Map.update!(:readPosition, &(&1 + 1))
+      |> Map.update!(:position, &(&1 + 1)), # TODO: should be set to readPosition
+      String.at(lexer.input, lexer.readPosition)
+    }
   end
 end
--- test/lexer_test.exs
+++ test/lexer_test.exs
@@ -2,7 +2,14 @@ defmodule LexerTest do
   use ExUnit.Case
   doctest Monkeylang.Lexer
 
-  test "greets the potato" do
-    assert Monkeylang.Lexer.hello() == :potato
+  alias Monkeylang.Lexer
+
+  test "test read_char" do
+    { lexer, char } = Lexer.new("+")
+    |> Monkeylang.Lexer.read_char()
+
+    assert char == "+"
+    assert lexer.readPosition == 1
+    assert lexer.position == lexer.readPosition
   end
 end
