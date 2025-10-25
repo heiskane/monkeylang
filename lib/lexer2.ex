db16998e65224903059e378d8e530ca2fda29197
Niko Heiskanen <henipe@gmail.com>
2025-10-25 13:02:31 +0300

    (wip): 2025-10-25T13:02

--- lib/lexer2.ex
+++ lib/lexer2.ex
@@ -29,6 +29,7 @@ defmodule Monkeylang.Lexer2 do
     end
   end
 
+  # TODO: handle `==` and `!=`
   def tokenize(input) do
     String.graphemes(input)
     |> do_tokenize([])
@@ -71,9 +72,9 @@ defmodule Monkeylang.Lexer2 do
     do_tokenize(tail, [token | tokens])
   end
 
-  defp read_number(input, number) when not is_digit(hd(input)), do: {number, input}
+  defp read_number(input = [ char | _tail ], number) when not is_digit(char), do: {number, input}
   defp read_number([char | tail], number), do: read_number(tail, number <> char)
 
-  defp read_ident(input, ident) when not is_letter(hd(input)), do: {ident, input}
+  defp read_ident(input = [ char | _tail ], ident) when not is_letter(char), do: {ident, input}
   defp read_ident([char | tail], ident), do: read_ident(tail, ident <> char)
 end
