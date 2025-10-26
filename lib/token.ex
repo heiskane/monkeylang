c6d821070e51c2f37a5224033d790035bbcf15eb
Niko Heiskanen <henipe@gmail.com>
2025-10-26 08:42:06 +0200

    (wip): 2025-10-26T08:42

--- lib/token.ex
+++ lib/token.ex
@@ -1,6 +1,12 @@
 defmodule Monkeylang.Token do
   defstruct [:type, :literal]
 
+  @type t :: %__MODULE__{
+          type: atom(),
+          literal: String.t()
+        }
+
+  @spec new(atom(), String.t()) :: t()
   def new(type, literal) do
     %__MODULE__{type: type, literal: literal}
   end
