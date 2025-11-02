eef4b09697c1555f634b2fed50d3017c83b5a7ba
Niko Heiskanen <henipe@gmail.com>
2025-11-02 18:33:58 +0200

    (wip): 2025-11-02T18:33

--- test/parser_test.exs
+++ test/parser_test.exs
@@ -42,11 +42,48 @@ defmodule ParserTest do
       true != false;
     """
 
-    {[node | _], _errors} =
+    {statements, _errors} =
       Monkeylang.Lexer.tokenize(input)
       |> Monkeylang.Parser.parse_tokens()
       |> dbg()
 
-    IO.puts(node)
+    expected = [
+      %Monkeylang.AST.InfixExpression{
+        token: %Monkeylang.Token{type: :equals, literal: "==", precedence: 1},
+        operator: "==",
+        left: %Monkeylang.AST.InfixExpression{
+          token: %Monkeylang.Token{type: :lt, literal: "<", precedence: 2},
+          operator: "<",
+          left: %Monkeylang.AST.Integer{
+            token: %Monkeylang.Token{type: :int, literal: "3", precedence: 0},
+            value: 3
+          },
+          right: %Monkeylang.AST.Integer{
+            token: %Monkeylang.Token{type: :int, literal: "5", precedence: 0},
+            value: 5
+          }
+        },
+        right: %Monkeylang.AST.Boolean{
+          token: %Monkeylang.Token{type: true, literal: "true", precedence: 0},
+          value: true
+        }
+      },
+      %Monkeylang.AST.InfixExpression{
+        token: %Monkeylang.Token{type: :notequals, literal: "!=", precedence: 1},
+        operator: "!=",
+        left: %Monkeylang.AST.Boolean{
+          token: %Monkeylang.Token{type: true, literal: "true", precedence: 0},
+          value: true
+        },
+        right: %Monkeylang.AST.Boolean{
+          token: %Monkeylang.Token{type: false, literal: "false", precedence: 0},
+          value: false
+        }
+      }
+    ]
+
+    # IO.puts(hd(statements))
+    # IO.puts(hd(tl(statements)))
+    assert statements == expected
   end
 end
